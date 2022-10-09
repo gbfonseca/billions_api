6ff014e39bae05889a14c9bff4e6976512614cf1
Gabriel Fonsecea <gabriel.fonsecadev@gmail.com>
2022-10-09 14:27:12 -0300

    feat: ensure CreateWalletController return 201 on create wallet

--- tests/src/controllers/create_wallet_controller_test.go
+++ tests/src/controllers/create_wallet_controller_test.go
@@ -8,8 +8,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"billions_api/main/src/controllers"
 	wallet "billions_api/main/src/domain/usecases/wallet"
+	"billions_api/main/src/presentation/controllers"
 	fixtures_test "billions_api/main/tests/fixtures"
 
 	"github.com/stretchr/testify/assert"
@@ -27,5 +27,22 @@ func TestShouldReturnBadRequestOnInvalidBody(t *testing.T) {
 	controllers.CreateWalletController(ctx)
 
 	assert.Equal(t, http.StatusBadRequest, w.Code)
+
+}
+
+func TestShouldReturnAnWalletOnSuccess(t *testing.T) {
+
+	w := httptest.NewRecorder()
+	addWallet := wallet.AddWallet{
+		Name: "My new wallet",
+	}
+	jsonValue, _ := json.Marshal(addWallet)
+
+	ctx := fixtures_test.GetTestGinContext(w)
+	ctx.Request.Body = io.NopCloser(bytes.NewBuffer(jsonValue))
+
+	controllers.CreateWalletController(ctx)
+
+	assert.Equal(t, http.StatusCreated, w.Code)
 
 }
