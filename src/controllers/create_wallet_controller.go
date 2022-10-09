290bd2327fbea3823c2443d65044a2de1e966742
Gabriel Fonsecea <gabriel.fonsecadev@gmail.com>
2022-10-09 04:02:50 -0300

    feat: should CreateWalletController return 400 on invalid body

---
+++ src/controllers/create_wallet_controller.go
@@ -0,0 +1,24 @@
+package controllers
+
+import (
+	"net/http"
+
+	wallet "billions_api/main/src/domain/usecases/wallet"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateWalletController(ctx *gin.Context) {
+
+	var requestBody wallet.AddWallet
+
+	err := ctx.ShouldBindJSON(&requestBody)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"xpto": "123",
+	})
+
+}
